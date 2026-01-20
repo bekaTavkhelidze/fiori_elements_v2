fe70a750666545d90306bce857fba6fafd64703f
BTavkhelidze <beka.tavkhelidze@leverx.com>
2026-01-20 16:25:46 +0400

    update app config add external link

--- webapp/ext/controller/ListReport.controller.js
+++ webapp/ext/controller/ListReport.controller.js
@@ -10,27 +10,20 @@ sap.ui.define(
     return ControllerExtension.extend(
       'fiorielementsv2.ext.controller.ListReport',
       {
-        override: {
-          onInit: function () {
-            console.log('Controller extension initialized');
-          },
-        },
-        onCheckBoxSelect: function (oEvent) {
-          console.log('first');
-          debugger;
-          // var oBindingParams = oEvent.getParameter('bindingParams');
+        filterItems: function (sValue) {
+          const oContext = this.base().getView();
+          console.log(oContext);
+          if (!sValue) {
+            return null;
+          }
 
-          // // ვპოულობთ ჩეკბოქსს ფრაგმენტის ID-ით
-          // var oCheckBox = this.byId('idFloorAreaCheckBox');
-
-          // if (oCheckBox && oCheckBox.getSelected()) {
-          //   // ვამატებთ ფილტრს: FloorArea > 1000
-          //   // "FloorArea" შეცვალეთ თქვენი OData პროპერტის სახელით
-          //   var oFilter = new Filter('FloorArea', FilterOperator.GT, 1000);
-          //   oBindingParams.filters.push(oFilter);
-          // }
+          return new Filter({
+            path: 'FloorArea',
+            operator: FilterOperator.GT,
+            value1: 2000,
+          });
         },
-      }
+      },
     );
-  }
+  },
 );
